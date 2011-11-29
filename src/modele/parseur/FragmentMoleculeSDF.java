3b2630d338573a2c29d2b5ce16137f47bcd6be50
G4llic4 <g4llic4@gmail.com>
2011-11-29 18:45:43 +0100

    new class

---
+++ src/modele/parseur/FragmentMoleculeSDF.java
@@ -0,0 +1,86 @@
+package modele.parseur;
+
+import java.util.LinkedList;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+
+public class FragmentMoleculeSDF
+{
+	// ----------------------------------------- //
+	// ----------------ATTRIBUTS---------------- //
+	// ----------------------------------------- //
+
+	public final static String	FIN_DESCRIPTION	= "$$$$";
+
+	private String				numDebutDescription;
+	private LinkedList<Float>	listeAtomes;
+	private LinkedList<Integer>	listeLiaisons;
+	private Hashtable			balises;
+
+	// ----------------------------------------- //
+	// --------------CONSTRUCTEURS-------------- //
+	// ----------------------------------------- //
+
+	public FragmentMoleculeSDF()
+	{
+
+	}
+
+	// ----------------------------------------- //
+	// -------------INITIALISEURS--------------- //
+	// ----------------------------------------- //
+
+	// ----------------------------------------- //
+	// -----------------METHODES---------------- //
+	// ----------------------------------------- //
+
+	// ----------------------------------------- //
+	// ---------------ACCESSEURS---------------- //
+	// ----------------------------------------- //
+
+	public String getNumDebutDescription()
+	{
+		return numDebutDescription;
+	}
+
+	public LinkedList<Float> getListeAtomes()
+	{
+		return listeAtomes;
+	}
+
+	public LinkedList<Integer> getLitseLiaisons()
+	{
+		return listeLiaisons;
+	}
+
+	public Hashtable getBalises()
+	{
+		return balises;
+	}
+
+	// ----------------------------------------- //
+	// ----------------MUTATEURS---------------- //
+	// ----------------------------------------- //
+
+	public void setNumDebutDescription(String numDebutDescription)
+	{
+		this.numDebutDescription = numDebutDescription;
+	}
+
+	public void setListeAtomes(LinkedList<Float> listeAtomes)
+	{
+		this.listeAtomes = listeAtomes;
+	}
+
+	public void setLitseLiaisons(LinkedList<Integer> listeLiaisons)
+	{
+		this.listeLiaisons = listeLiaisons;
+	}
+
+	public void setBalises(Hashtable balises)
+	{
+		this.balises = balises;
+	}
+
+
+}
